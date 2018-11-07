b3fd38b5803c06623c3b76cd46ed37ce6e896016
mdshun <hoang_van_tuan@mediado.jp>
2018-11-08 08:33:09 +0900

    update zip

--- zip.go
+++ zip.go
@@ -3,77 +3,68 @@ package main
 import (
 	"archive/zip"
 	"io"
-	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
-func zipFiles(output, input string) error {
-	dest, err := os.Create(output)
-	if err != nil {
-		panic(err)
-	}
-
-	zipWriter := zip.NewWriter(dest)
-	defer zipWriter.Close()
-
-	return addToZips(input, zipWriter)
-}
-
-func addToZips(path string, zipWriter *zip.Writer) error {
-	inputInfo, err := os.Stat(path)
-
+func zipit(source, target string) error {
+	zipfile, err := os.Create(target)
 	if err != nil {
 		return err
 	}
+	defer zipfile.Close()
 
-	if inputInfo.IsDir() {
-		fileInfos, err := ioutil.ReadDir(inputInfo.Name())
+	archive := zip.NewWriter(zipfile)
+	defer archive.Close()
+
+	info, err := os.Stat(source)
+	if err != nil {
+		return nil
+	}
+
+	var baseDir string
+	if info.IsDir() {
+		baseDir = filepath.Base(source)
+	}
+
+	filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		for _, fileInfo := range fileInfos {
-			if fileInfo.IsDir() {
-				err := addToZips(fileInfo.Name(), zipWriter)
-
-				if err != nil {
-					return err
-				}
-			} else {
-				addToZip(fileInfo.Name(), zipWriter)
-
-				if err != nil {
-					return err
-				}
-			}
-		}
-	} else {
-		err := addToZip(inputInfo.Name(), zipWriter)
-
+		header, err := zip.FileInfoHeader(info)
 		if err != nil {
 			return err
 		}
-	}
 
-	return nil
-}
+		if baseDir != "" {
+			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
+		}
 
-func addToZip(filename string, zipWriter *zip.Writer) error {
-	src, err := os.Open(filename)
-	if err != nil {
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			header.Method = zip.Deflate
+		}
+
+		writer, err := archive.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		_, err = io.Copy(writer, file)
 		return err
-	}
-	defer src.Close()
+	})
 
-	writer, err := zipWriter.Create(filename)
-	if err != nil {
-		return err
-	}
-
-	_, err = io.Copy(writer, src)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
